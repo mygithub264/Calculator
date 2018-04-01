73b29eef67dc67307cfbf119dc9b44bba1ca92a6
mygithub264 <37976217+mygithub264@users.noreply.github.com>
2018-04-01 05:13:44 -0700

    Calculator project added

---
+++ script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by abhilash on 10/3/2017.
+ */
+function btnplus() {
+    document.calculator.display.value +="+";
+    document.calculator.display.style.textAlign="right";
+}
+function btnsub() {
+    document.calculator.display.value +="-";
+    document.calculator.display.style.textAlign="right";
+}
+function btnmult() {
+    document.calculator.display.value +="*";
+    document.calculator.display.style.textAlign="right";
+}
+function btnmod() {
+    document.calculator.display.value +="%";
+    document.calculator.display.style.textAlign="right";
+}
+function btndot() {
+    document.calculator.display.value +=".";
+    document.calculator.display.style.textAlign="right";
+}
+function btndiv() {
+    document.calculator.display.value +="/";
+    document.calculator.display.style.textAlign="right";
+}
+function btnclear() {
+    document.calculator.display.value ="";
+    document.calculator.display.style.textAlign="right";
+}
+
